15efff1bf328c6673342e1abc49af0cdf38ab687
thisbit <thisbit@github.com>
2020-04-13 11:15:41 +0200

    included theme chooser in script

--- wpinit.sh
+++ wpinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# ASK FOR VARIABLES
+# ASK FOR USER INPUT
 	# WARNING
-		echo  "# WARNING: is this the folder you want to install in?"
+		echo  "# WARNING: run this in your webroot!"
 	# the project title
 		read -p "# PROJECT TITLE: " project
 
@@ -10,6 +10,13 @@
 
 	# the db password
 		read -p "# PASSWORD (for wp and mysql to use): " pass
+
+		read -p "# CHOOSE THEME (write number):
+		(1) wprig (starter theme with build processes, linting etc),
+		(2) _s underscores (simple starter theme),
+		(3) understrap (_s with bootstrap),
+		(4) enfold (commercial theme with blockeditor, a parent theme)
+		: " theme
 
 	# open in browser (firefox, chromium, opera, qutebrowser):
 		read -p "# YOUR BROWSER (firefox, chromium, opera, qutebrowser): " browser
@@ -84,11 +91,54 @@
 		rm -rf master.zip
 ​
 # SET WPRIG
-	wget https://github.com/wprig/wprig/archive/master.zip
-	unzip master.zip
-	mv wprig-master ./$project/wp-content/themes/wprig-$project
-	rm -rf master.zip
+	# wget https://github.com/wprig/wprig/archive/master.zip
+	# unzip master.zip
+	# mv wprig-master ./$project/wp-content/themes/wprig-$project
+	# rm -rf master.zip
 	# get js dependencies with gulpfile (I think composer needs to be global on machine)
+
+# SET THEME
+	enfold_child_css_header="/*
+	Theme Name: project
+	Author: thisbit
+	Template: enfold
+	Author URI: https://github.com/thisbit/
+	Description: Lorem Ipsum
+	Version: 1.0
+	Text Domain: project_
+	Tags: lorem, ipsum
+	*/
+	"
+
+	if [[ $theme = "1" ]]
+		then
+		wget https://github.com/wprig/wprig/archive/master.zip
+		unzip master.zip
+		mv wprig-master ./$project/wp-content/themes/wprig-$project
+		rm -rf master.zip
+	elif [ $theme = "2" ]
+		then
+		wget https://github.com/Automattic/_s/archive/master.zip
+		unzip master.zip
+		mv _s-master ./$project/wp-content/themes/_s-$project
+		rm -rf master.zip
+		# see if you can automate string replacement "_s" with "$project"
+	elif [ $theme = "3" ]
+		then
+		wget https://github.com/understrap/understrap/archive/master.zip
+		unzip master.zip
+		mv understrap-master ./$project/wp-content/themes/understrap-$project
+	elif [ $theme = "4" ]
+		then
+		unzip /home/ek/serv/enfold-2020/enfold.zip -d ./$project/wp-content/themes/
+		mkdir ./$project/wp-content/themes/enfold-child/
+		echo "$enfold_child_css_header" > ./$project/wp-content/themes/enfold-child/styles.css
+		sed -i "s/project/$project/g" ./$project/wp-content/themes/enfold-child/styles.css
+	else
+		echo "ERROR, you failed to choose an available theme!"
+		exit
+	fi
+	echo "theme successfully installed"
 
 # FIX PROJECT PREFERENCES (with user that is part of group www-data or _www (on mac))
 	sudo chown -Rf www-data:www-data ./$project
